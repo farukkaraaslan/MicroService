8c3847a47857b553dcb5b51214a0d1d0e546c3b6
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-25 13:56:29 +0300

    serviceler oluşturuldu

---
+++ CustomerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "Hilmi CELAYİR", "Saniye YILDIZ", "Faruk KARAASLAN", "Ramazan TAŞÖRER","Oğuzhan DİLEK" });
+        }
+    }
+}
---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "Kalem", "Kitap","Silgi", "Defter"});
+        }
+    }
+}
