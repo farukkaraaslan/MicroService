f9da4641b94a700be0cae00edc1314fc01463996
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-26 11:08:23 +0300

    docker file oluşturuldu

--- CustomerAPI/Controllers/CustomersController.cs
+++ CustomerAPI/Controllers/CustomersController.cs
@@ -7,10 +7,25 @@ namespace CustomerAPI.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private IConfiguration _configuration;
+
+        public CustomersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new List<string> { "Hilmi CELAYİR", "Saniye YILDIZ", "Faruk KARAASLAN", "Ramazan TAŞÖRER","Oğuzhan DİLEK" });
+            var data = _configuration["data"];
+            return Ok(string.Concat(data," - ", 
+                new List<string> { 
+                    "Hilmi CELAYİR",
+                    "Saniye YILDIZ", 
+                    "Faruk KARAASLAN", 
+                    "Ramazan TAŞÖRER", 
+                    "Oğuzhan DİLEK" 
+                }));
         }
     }
 }
