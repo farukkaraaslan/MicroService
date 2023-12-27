c31bdb024450d724c40ab0628691cfb7a72bdea0
farukkaraaslan <faruk.karaaslan.55@gmail.com>
2023-12-27 14:01:39 +0300

    servce caller yazıldı

---
+++ CustomerAPI/Helper/Serv.cs
@@ -0,0 +1,28 @@
+﻿using CustomerAPI.Interfaces;
+
+namespace CustomerAPI.Helper;
+
+public class ServiceHelper : IServiceCallHelper
+{
+    public Task<object> Get(string uri)
+    {
+      
+    }
+
+    public async Task<string> Post(Uri uri, HttpMethod method, StringContent stringContent)
+    {
+        using (var client = new HttpClient())
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                Content = stringContent,
+                RequestUri = uri
+            };
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        }
+    }
+}
---
+++ CustomerAPI/Interfaces/IServiceCallHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomerAPI.Interfaces;
+
+public interface IServiceCallHelper
+{
+    Task<string> Post(Uri uri, HttpMethod method, StringContent stringContent);
+    Task<object> Get(string uri);
+}
